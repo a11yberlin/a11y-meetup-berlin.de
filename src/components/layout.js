2521ec93b88443c425e6af62d14592ec484fa383
radibit <radimir.bitsov@gmail.com>
2019-02-17 13:34:09 +0100

    ✨add a highlight component, update the logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types'
 
 import Header from './header'
 import Footer from './footer'
+import Highlight from './highlight'
 import '../styles/base.css'
 import '../styles/helper.css'
 import '../styles/layout.css'
+
+const highlightContent = {
+  title: 'Code of Conduct',
+  content: `If you are attending, organizing or speaking at our event, you are
+    required to agree with our`,
+  link: {
+    to: '/code-of-conduct',
+    text: 'Code of Conduct',
+  },
+}
 
 const Layout = ({ children }) => (
   <>
@@ -23,6 +34,11 @@ const Layout = ({ children }) => (
     <main id="main" className="l-container l-mainContent">
       {children}
     </main>
+    <Highlight
+      title={highlightContent.title}
+      content={highlightContent.content}
+      link={highlightContent.link}
+    />
     <Footer />
   </>
 )
